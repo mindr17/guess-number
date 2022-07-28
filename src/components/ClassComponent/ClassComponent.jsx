aee1f5ebe82722e95e9aa86023a6b8f66d25861f
mindr17 <mindr17@yandex.ru>
2022-07-28 13:40:27 +0300

    feat: propTypes

---
+++ src/components/ClassComponent/ClassComponent.jsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import style from './ClassComponent.module.css';
+import PropTypes from 'prop-types';
+
+export class ClassComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      number: 5,
+      userNumber: '',
+      randomNumber:
+        Math.floor(Math.random() * this.props.max - this.props.min) +
+        this.props.min,
+        count: 0,
+    };
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    this.setState({
+      count: this.state.count
+    })
+
+    this.setState(state => {
+      if (!state.userNumber) {
+        return {
+          result: `Введите число`,
+        };
+      }
+
+      if (state.userNumber > state.randomNumber) {
+        return {
+          result: `${state.userNumber} больше загаданного`,
+        };
+      }
+
+      if (state.userNumber < state.randomNumber) {
+        return {
+          result: `${state.userNumber} меньше загаданного`,
+        };
+      }
+
+      return {
+        result: `Вы угадали, загаданное число ${state.userNumber},
+          попыток ${state.count}`,
+      };
+    });
+  };
+
+  handleChange = e => {
+    this.setState((state, props) => ({
+      userNumber: e.target.value,
+    }));
+
+    setTimeout(() => {
+      console.log(this.state);
+    }, 0);
+  };
+
+  render() {
+    return (
+      <div className={style.game}>
+        <p className={style.result}>{this.state.number}</p>
+        <form className={style.form} onSubmit={this.handleSubmit}>
+          <label className={style.label} htmlFor='user_number'>
+            Угадай число
+          </label>
+          <input className={style.input} type='number' id='user_number'
+            onChange={this.handleChange} value={this.state.user} />
+          <button className={style.btn}>Угадать</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+ClassComponent.PropTypes = {
+  min: PropTypes.number,
+  max: PropTypes.number,
+};
